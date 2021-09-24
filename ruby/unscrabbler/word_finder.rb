d53aa7dbe96ef7f03e2308bd2e9b0dbbf4681e95
DanHansens <daniel.hansens@gmail.com>
2021-09-23 20:15:34 -0500

    Refactor

--- ruby/unscrabbler/word_finder.rb
+++ ruby/unscrabbler/word_finder.rb
@@ -3,38 +3,40 @@
 class WordFinder
   def find(letters)
     @max_word_length = letters.size
-    contents = File.read('dictionary.json')
-    dictionary = Kernel.eval(contents)
-    chars = letters.split('')
+    dictionary = Kernel.eval(File.read('dictionary.json'))
 
+    parse_letters(letters)
+
+    matched_words = []
+    dictionary.each do |word|
+      matched_words << word if validate_word(word)
+    end
+
+    matched_words
+  end
+
+  def validate_word(word)
+    return false if word.size > @max_word_length
+
+    found_letters = 0
+
+    @letter_count.each do |letter, num|
+      return false if word.count(letter) > num
+
+      found_letters += word.count(letter)
+    end
+    found_letters == word.size
+  end
+
+  def parse_letters(letters)
     @letter_count = {}
 
-    chars.each do |c|
+    letters.split('').each do |c|
       if @letter_count.key? c
         @letter_count[c] += 1
       else
         @letter_count[c] = 1
       end
     end
-
-    matched_words = []
-    dictionary.each do |word|
-      matched_words << word if check_word(word)
-    end
-
-    matched_words
-  end
-
-  def check_word(word)
-    return false if word.size > @max_word_length
-
-    size = 0
-
-    @letter_count.each do |letter, num|
-      return false if word.count(letter) > num
-
-      size += word.count(letter)
-    end
-    size >= word.size
   end
 end
